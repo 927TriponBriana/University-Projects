ec48a3b686f259925e8e4ba3b9918f026851c613
TriponBriana <91745099+927TriponBriana@users.noreply.github.com>
2023-10-24 11:59:59 +0300

    Add files via upload

---
+++ Semester 5/FLCD/Lab2/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    public static void main(String[] args) {
+        SymbolTable<String> identifierSymbolTable = new SymbolTable<>();
+        SymbolTable<Integer> constantSymbolTable = new SymbolTable<>();
+
+        identifierSymbolTable.add("x");
+        identifierSymbolTable.add("y");
+        constantSymbolTable.add(1);
+        constantSymbolTable.add(6);
+
+        System.out.println("Contains 'x': " + identifierSymbolTable.contains("x"));
+        System.out.println("Contains 'z': " + identifierSymbolTable.contains("z"));
+
+        System.out.println("Contains 1: " + constantSymbolTable.contains(1));
+        System.out.println("Contains 6: " + constantSymbolTable.contains(6));
+
+        int position = constantSymbolTable.getPosition(1);
+        if(position != -1) {
+            System.out.println("Position of 1 is: " + position + "; in bucket: " + constantSymbolTable.getBucketPosition(1));
+        }
+        else{
+            System.out.println("Value not found!");
+        }
+        int position1 = constantSymbolTable.getPosition(6);
+        if(position1 != -1) {
+            System.out.println("Position of 6 is: " + position1 + "; in bucket: " + constantSymbolTable.getBucketPosition(6));
+        }
+        else{
+            System.out.println("Value not found!");
+        }
+
+        System.out.println(identifierSymbolTable.getHashTable());
+        System.out.println(constantSymbolTable.getHashTable());
+    }
+}
---
+++ Semester 5/FLCD/Lab2/SymbolTable.java
@@ -0,0 +1,47 @@
+import java.util.Hashtable;
+import java.util.List;
+
+public class SymbolTable<Value> {
+    private CustomHashTable<Value> hashTable;
+
+    public SymbolTable() {
+        this.hashTable = new CustomHashTable<>();
+    }
+
+    public void add(Value value) {
+        hashTable.addToHashTable(value);
+    }
+
+    public boolean contains(Value value) {
+        return hashTable.contains(value);
+    }
+
+//    public void delete(Value value) {
+//        hashTable.deleteFromHashTable(value);
+//    }
+
+    public boolean isEmpty() {
+        return hashTable.isEmpty();
+    }
+
+    public int getSize() {
+        return hashTable.size();
+    }
+
+    public int getPosition(Value value) {
+        List<Value> allValues = hashTable.getAllValues();
+        if(allValues.contains(value)) {
+            return allValues.indexOf(value);
+        }
+        return -1; //if value does not exist
+    }
+
+    public int getBucketPosition(Value value) {
+        int index = hashTable.getBucketPosition(value);
+        return index;
+    }
+
+    public CustomHashTable getHashTable(){
+        return hashTable;
+    }
+}
