d422c153849b22d837b89eed2e989f3f736a98ee
TriponBriana <91745099+927TriponBriana@users.noreply.github.com>
2023-11-07 15:02:12 +0200

    Update Main.java

--- Semester 5/FLCD/Lab3-4/Main.java
+++ Semester 5/FLCD/Lab3-4/Main.java
@@ -9,39 +9,6 @@ public class Main {
         String prg3 = "p3.txt";
         String prgerr = "p1err.txt";
 //        scanner.print();
-        //scanner.scan(prg1);
-        //scanner.scan(prg2);
-        //scanner.scan(prg3);
         scanner.scan(prg1);
-//        SymbolTable identifierSymbolTable = new SymbolTable(5);
-//        SymbolTable intConstantSymbolTable = new SymbolTable(5);
-//        SymbolTable stringConstantSymbolTable = new SymbolTable(5);
-//
-//        try {
-//            identifierSymbolTable.addIdentifier("x");
-//            identifierSymbolTable.addIdentifier("z");
-//            intConstantSymbolTable.addIntConstant(1);
-//            intConstantSymbolTable.addIntConstant(6);
-//            intConstantSymbolTable.addIntConstant(6);
-//        } catch (Exception e) {
-//            System.out.println("Error: " + e.getMessage());
-//        }
-//
-//        if(intConstantSymbolTable.hasIntConstant(1)) {
-//            System.out.println("Contains 1");
-//        } else {
-//            System.out.println("Does not contain 1");
-//        }
-//
-//        int position = intConstantSymbolTable.getIntConstantPosition(1);
-//        System.out.println("Position of 1 is: " + position);
-//
-//        int position1 = intConstantSymbolTable.getIntConstantPosition(6);
-//        System.out.println("Position of 6 is: " + position1);
-//
-//        System.out.println(identifierSymbolTable.getIdentifierHashTable());
-//        System.out.println(intConstantSymbolTable.getIntConstHashTable());
-//        System.out.println(stringConstantSymbolTable.getStringConstHashTable());
-
     }
 }
