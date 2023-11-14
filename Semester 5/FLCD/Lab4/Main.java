eb136a34447f09e88c4c5e95a323f36c8d062801
TriponBriana <91745099+927TriponBriana@users.noreply.github.com>
2023-11-14 12:29:36 +0200

    Add files via upload

---
+++ Semester 5/FLCD/Lab4/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        MyScanner scanner = new MyScanner();
+        String prg1 = "p1.txt";
+        String prg2 = "p2.txt";
+        String prg3 = "p3.txt";
+        String prgerr = "p1err.txt";
+//        scanner.print();
+        //scanner.scan(prg1);
+        //scanner.scan(prg2);
+        //scanner.scan(prg3);
+        scanner.scan(prgerr);
+    }
+}
