84b0dea6bba40318bb13b1f1e64a0d2aabbde211
TriponBriana <91745099+927TriponBriana@users.noreply.github.com>
2024-01-16 15:46:55 +0200

    Add files via upload

--- Semester 5/FLCD/Lab7_final/Main.java
+++ Semester 5/FLCD/Lab7_final/Main.java
@@ -1,59 +1,81 @@
+package FA;
+
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-//        Grammar grammar = new Grammar("g1.txt");
-        Grammar grammar = new Grammar("g1.txt");
-        grammar.initializeFromFile();
+    public static void main(String[] args) {
+        try{
+            FiniteAutomata fa = new FiniteAutomata();
+            fa.readFromFile("src/programs/fa.txt");
 
+            Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+            int choice;
+            do {
+                System.out.println("\nMenu:");
+                System.out.println("1. Display States");
+                System.out.println("2. Display Alphabet");
+                System.out.println("3. Display Transitions");
+                System.out.println("4. Display Initial State");
+                System.out.println("5. Display Final States");
+                System.out.println("6. Verify if DNF");
+                System.out.println("7. Verify Sequence");
+                System.out.println("8. Exit");
+                System.out.print("Enter your choice: ");
+                choice = scanner.nextInt();
 
-        while (true) {
-            System.out.println("\n");
-            System.out.println("1. Print Nonterminals");
-            System.out.println("2. Print Terminals");
-            System.out.println("3. Print Productions");
-            System.out.println("4. Print Productions for a given Nonterminal");
-            System.out.println("5. Check is the grammar is CFG");
-            System.out.println("0. Exit");
-
-            System.out.println("Enter option: ");
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (choice) {
-                case 1:
-                    grammar.printNonTerminals();
-                    break;
-                case 2:
-                    grammar.printTerminals();
-                    break;
-                case 3:
-                    grammar.printProductions();
-                    break;
-                case 4:
-                    System.out.print("Enter a nonterminal to print its productions: ");
-                    String userNonterminal = scanner.nextLine();
-                    grammar.printProductionsForNonterminal(userNonterminal);
-                    break;
-                case 5:
-                    if (grammar.checkIfCFG()) {
-                        System.out.println("The grammar is a CFG");
-                    } else {
-                        System.out.println("The grammar is not a CFG");
-                    }
-                    break;
-                case 0:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid option!");
-
-            }
+                switch (choice) {
+                    case 1:
+                        fa.printStates();
+                        break;
+                    case 2:
+                        fa.printAlphabet();
+                        break;
+                    case 3:
+                        fa.printTransitions();
+                        break;
+                    case 4:
+                        fa.printInitialState();
+                        break;
+                    case 5:
+                        fa.printFinalStates();
+                        break;
+                    case 6:
+                        System.out.println(fa.checkIfDFA());
+                        break;
+                    case 7:
+//                        System.out.print("Enter the sequence to verify: ");
+//                        String sequence = System.console().readLine();
+//                        System.out.println("Sequence is accepted: " + dfa.isAccepted(sequence));
+                        var word = new Scanner(System.in).nextLine();
+                        System.out.println(fa.isAccepted(word));
+                        break;
+                    case 8:
+                        System.out.println("Exiting program...");
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please enter a valid option.");
+                        break;
+                }
+            } while (choice != 8);
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
         }
-
     }
 }
+
+//DFA
+//states p, q, r
+//alphabet 0, 1
+//transitions p, 1, q; p, 0, p; q, 0, r; q, 1, p; r, 0, q; r, 1, r
+//initial p
+//finals r
+
+
+//not DFA
+//states a, b, c
+//alphabet 0, 1
+//transitions a, 0, a; b, 1, a; c, 1, c; c, 1, b
+// initial a
+//finals a, c
